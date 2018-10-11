3c5e8b0ca034a382b320483aa1922cf6768b2fa7
Mate Papp <matepapp@icloud.com>
2018-10-11 17:50:23 +0200

    Import apollo-link to handle authorization headers

--- src/index.js
+++ src/index.js
@@ -2,19 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
+import { setContext } from 'apollo-link-context'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { BrowserRouter } from 'react-router-dom'
 
 import './styles/index.css'
 import App from './components/App'
+import { AUTH_TOKEN } from './constants'
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer: ${token}` : ''
+    }
+  }
+})
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
