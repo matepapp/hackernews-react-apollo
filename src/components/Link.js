31aa1765bb7f5a324d5e25b51e2fe78e7eb5016b
Mate Papp <matepapp@icloud.com>
2018-10-11 18:27:54 +0200

    Add live tracking after vote

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,12 +1,63 @@
 import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+import { AUTH_TOKEN } from '../constants'
+import { timeDifferenceForDate } from '../utils'
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
 
 class Link extends Component {
   render() {
-    const { description, url } = this.props.link
+    const { link, index } = this.props
+    const { description, url, votes, postedBy, createdAt } = link
     const title = `${description} (${url})`
+    const authToken = localStorage.getItem(AUTH_TOKEN)
+
     return (
-      <div>
-        <div>{title}</div>
+      <div className="flex mt2 items-start">
+        <div className="flex items-center">
+          <span className="gray">{index + 1}.</span>
+          {authToken && (
+            <Mutation
+              mutation={VOTE_MUTATION}
+              variables={{ linkId: link.id }}
+              update={(store, { data: { vote } }) =>
+                this.props.updateStoreAfterVote(store, vote, link.id)
+              }
+            >
+              {voteMutation => (
+                <div className="ml1 gray f11" onClick={voteMutation}>
+                  ▲
+                </div>
+              )}
+            </Mutation>
+          )}
+        </div>
+        <div className="ml1">
+          <div>{title}</div>
+          <div className="f6 lh-copy gray">
+            {votes.length} votes | by {postedBy ? postedBy.name : 'Unknown'}{' '}
+            {timeDifferenceForDate(createdAt)}
+          </div>
+        </div>
       </div>
     )
   }
